4aecb8f8074649dfe1b59915fd6af20ee93aa29b
israt <israt9528@gmail.com>
2026-05-07 17:24:17 +0600

    remove console log

--- solutions.ts
+++ solutions.ts
@@ -3,13 +3,11 @@
 function filterEvenNumbers(array: number[]): number[] {
   return array.filter((num) => num % 2 == 0);
 }
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
 // problem-2
 function reverseString(param: string): string {
   return param.split("").reverse().join("");
 }
-console.log(reverseString("typescript"));
 
 // problem-3
 type StringOrNumber = string | number;
@@ -17,15 +15,11 @@ function checkType(param: StringOrNumber): string {
   if (typeof param === "string") return "String";
   else return "Number";
 }
-console.log(checkType("Hello"));
-console.log(checkType(42));
 
 // problem-4
 function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
 }
-const user = { id: 1, name: "John Doe", age: 21 };
-console.log(getProperty(user, "name"));
 
 // problem-5
 interface Book {
@@ -36,12 +30,6 @@ interface Book {
 function toggleReadStatus(book: Book): Book & { isRead: boolean } {
   return { ...book, isRead: true };
 }
-const myBook = {
-  title: "TypeScript Guide",
-  author: "Jane Doe",
-  publishedYear: 2024,
-};
-console.log(toggleReadStatus(myBook));
 
 // problem-6
 class Person {
@@ -64,11 +52,8 @@ class Student extends Person {
     return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
   }
 }
-const student = new Student("Alice", 20, "A");
-console.log(student.getDetails());
 
 // problem-7
 function getIntersection(param1: number[], param2: number[]): number[] {
   return param1.filter((num) => param2.includes(num));
 }
-console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
